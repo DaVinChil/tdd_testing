826534445def82223920e2aba1f411e8c6feddd4
DaVinChi <fox.winner9@gmail.com>
2023-04-06 17:28:08 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,7 +1,12 @@
 package org.example;
+
+import java.util.HashMap;
+
 public class PhoneBook {
+    private HashMap<String, String> phoneList = new HashMap<>();
 
     public int add(String name, String phone){
-        return 0;
+        phoneList.putIfAbsent(name, phone);
+        return phoneList.size();
     }
 }
