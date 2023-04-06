de79b1f3f247caf39c3a13e12039317fdb5e6b98
DaVinChi <fox.winner9@gmail.com>
2023-04-06 16:28:11 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,38 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 public class PhoneBookTest {
-    
+
+    @Test
+    void addOneContact(){
+        PhoneBook phoneBook = new PhoneBook();
+        int actual = phoneBook.add("Alex", "139024932");
+        int expect = 1;
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test
+    void addSameContacts(){
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Alex", "12345");
+        int actual = phoneBook.add("Alex", "79563");
+        int expect = 1;
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test
+    void addALotContacts(){
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Ilya", "90234");
+        phoneBook.add("Sasha", "30924");
+        phoneBook.add("Dariya", "0293482");
+        phoneBook.add("Arseniy", "123283");
+        phoneBook.add("Mariya", "942482");
+        phoneBook.add("Mariya", "432524");
+        int actual = phoneBook.add("Vadim", "354924");
+        int expect = 6;
+        Assertions.assertEquals(expect, actual);
+    }
 }
