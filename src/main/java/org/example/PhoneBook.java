94346d3768dc421c9bdcd9653716f10a18cbbc06
DaVinChi <fox.winner9@gmail.com>
2023-04-06 17:45:34 +0300

    findByNumber implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -4,13 +4,18 @@ import java.util.HashMap;
 
 public class PhoneBook {
     private HashMap<String, String> phoneList = new HashMap<>();
+    private HashMap<String, String> nameList = new HashMap<>();
 
     public int add(String name, String phone){
-        phoneList.putIfAbsent(name, phone);
+        if(phoneList.get(name)==null && nameList.get(phone)==null){
+            phoneList.put(name, phone);
+            nameList.put(phone, name);
+        }
+
         return phoneList.size();
     }
 
     public String findByNumber(String number){
-        return null;
+        return nameList.get(number);
     }
 }
